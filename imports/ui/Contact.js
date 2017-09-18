067eb17263f3cc123e8bb547f30790653eca84eb
Christian Najera <chris_najera@unitec.edu>
2017-09-18 15:39:14 -0600

    design

--- imports/ui/Contact.js
+++ imports/ui/Contact.js
@@ -8,6 +8,9 @@ export default class Contact extends React.Component {
                 <div className="col-lg-8 col-lg-offset-2">
                   <h2>Contact Peko</h2>
                   <p>Your easy way to experience all the vast spectrum that Honduras has to offer in tourism</p>
+                  <a href="#" className="fa fa-twitter"></a>
+                  <a href="#" className="fa fa-facebook"></a>
+                  <a href="https://instagram.com/pekohn" target="_blank" className="fa fa-instagram"></a>
                 </div>
             </div>
         </section>
