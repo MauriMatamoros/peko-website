62ef1231489bf9ba28b5eda6f02e4c8fc24be24c
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-09-18 15:48:34 -0600

    Fixing About List

--- imports/ui/About.js
+++ imports/ui/About.js
@@ -7,9 +7,18 @@ export default class About extends React.Component {
           <div className="row">
               <div className="col-lg-8 col-lg-offset-2">
                 <h2>About Peko</h2>
-                <p>Visiting us for a new experience in your life.</p>
-                <p>Subscribe now and you will receive information about special tours.</p>
-                <p>Honduras has many great places, like the second largest coral reef in the world!.</p>
+                <ul id="about-list">
+                  <li>
+                    <p>Visiting us for a new experience in your life.</p>
+                  </li>
+                  <li>
+                    <p>Subscribe now and you will receive information about special tours.</p>
+                  </li>
+                  <li>
+                    <p>Honduras has many great places, like the second largest coral reef in the world!.</p>
+
+                  </li>
+                </ul>
               </div>
           </div>
       </section>
